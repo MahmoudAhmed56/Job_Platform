90111bcd0249718b25d9ca99aad0035250a983bd
MahmoudAhmed56 <mahmoudcs123@gmail.com>
2025-04-05 18:45:22 +0200

    add arcjet security to createCompany and createJobSeeker actions

--- app/actions.ts
+++ app/actions.ts
@@ -5,9 +5,24 @@ import { requireUser } from "./utils/requireUser";
 import { z } from "zod";
 import { prisma } from "@/lib/prisma";
 import { redirect } from "next/navigation";
+import arcjet, { detectBot, shield } from "./utils/arcjet";
+import { request } from "@arcjet/next";
+
+const aj = arcjet.withRule(shield({})).withRule(
+  detectBot({
+    mode: "LIVE",
+    allow: ["CATEGORY:SEARCH_ENGINE"],
+  })
+);
 
 export async function createCompany(data: z.infer<typeof companySchema>) {
   const session = await requireUser();
+
+  const req = await request();
+  const decision = await aj.protect(req);
+  if (decision.isDenied()) {
+    throw new Error("Forbidden");
+  }
 
   const validateData = companySchema.safeParse(data);
   if (!validateData.success) {
@@ -31,6 +46,13 @@ export async function createCompany(data: z.infer<typeof companySchema>) {
 
 export async function createJobSeeker(data: z.infer<typeof jobSeekerSchema>) {
   const user = await requireUser();
+
+  const req = await request();
+  const decision = await aj.protect(req);
+  if (decision.isDenied()) {
+    throw new Error("Forbidden");
+  }
+
   const validateData = jobSeekerSchema.parse(data);
   await prisma.user.update({
     where: {
