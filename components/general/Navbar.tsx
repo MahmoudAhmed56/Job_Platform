47c76a7352bccb6af8500915a1f6ebaf578f0c32
MahmoudAhmed56 <mahmoudcs123@gmail.com>
2025-04-06 21:52:10 +0200

    add user dropdown menu

--- components/general/Navbar.tsx
+++ components/general/Navbar.tsx
@@ -2,27 +2,33 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import Logo from "@/public/logo.png";
-import { Button, buttonVariants } from "../ui/button";
+import { buttonVariants } from "../ui/button";
 import { ThemeToggle } from "./ThemeToggle";
-import { auth, signOut } from "@/app/utils/auth";
+import { auth } from "@/app/utils/auth";
+import UserDropdown from "./UserDropdown";
 
 const Navbar = async () => {
   const session = await auth();
   return (
     <nav className="flex justify-between items-center py-5">
       <Link href="/" className="flex items-center gap-2">
-      <Image src={Logo} alt="Chad Job Logo" width={40} height={40} />
-        <h1 className="text-2xl font-bold">Job<span className="text-primary">Chad</span></h1>
+        <Image src={Logo} alt="Chad Job Logo" width={40} height={40} />
+        <h1 className="text-2xl font-bold">
+          Job<span className="text-primary">Chad</span>
+        </h1>
       </Link>
-      <div className="flex items-center gap-4">
-      <ThemeToggle />
-      {session?.user ? (
-          <form action={async () => {
-            "use server"
-            await signOut({redirectTo:"/"})
-          }}>
-            <Button>Logout</Button>
-          </form>
+      {/* Desktop Navigation */}
+      <div className="hidden md:flex items-center gap-5">
+        <ThemeToggle />
+        <Link href="/post-job" className={buttonVariants({ size: "lg" })}>
+          Post Job
+        </Link>
+        {session?.user ? (
+          <UserDropdown
+            email={session.user.email as string}
+            name={session.user.name as string}
+            image={session.user.image as string}
+          />
         ) : (
           <Link
             href="/login"
