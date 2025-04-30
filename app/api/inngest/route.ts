3e868135d5f3f5ae001278b799b802468f955bdb
MahmoudAhmed56 <mahmoudcs123@gmail.com>
2025-04-30 13:25:22 +0200

    add sendPeriodicJobListing function for job notifications and integrate Resend service

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -1,12 +1,13 @@
 import { inngest } from "@/app/utils/inngest/client";
 import { serve } from "inngest/next";
-import { handelExpiration, helloWorld } from "./functions";
+import { handelExpiration, helloWorld, sendPeriodicJobListing } from "./functions";
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
     helloWorld,
-    handelExpiration
+    handelExpiration,
+    sendPeriodicJobListing
   ],
 });
