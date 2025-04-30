4e58181993a893537c67bc9d4e695704c346df1c
MahmoudAhmed56 <mahmoudcs123@gmail.com>
2025-04-30 11:10:14 +0200

    add JobListing component with data fetching and EmptyState handling

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -4,6 +4,10 @@ export default function Home() {
   return (
     <div className="grid grid-cols-3 gap-8">
       <JobFilter />
+      <div className="col-span-2 flex flex-col gap-6">
+        
+      </div>
+
     </div>
   );
 }
---
+++ components/general/JobListing.tsx
@@ -0,0 +1,56 @@
+import { prisma } from "@/lib/prisma";
+import EmptyState from "./EmptyState";
+
+async function getData() {
+  const data = await prisma.jobPost.findMany({
+    where: {
+      status: "ACTIVE",
+    },
+    select: {
+      jobTitle: true,
+      id: true,
+      salaryFrom: true,
+      salaryTo: true,
+      employmentType: true,
+      location: true,
+      createdAt: true,
+      company: {
+        select: {
+          name: true,
+          logo: true,
+          location: true,
+          about: true,
+        },
+      },
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+  return data;
+}
+const JobListing = async () => {
+  const data = await getData();
+  return (
+    <>
+      {data.length > 0 ? (
+        <div className="flex flex-col gap-6">
+          {data.map((job) => (
+            <p className="" key={job.id}>
+              {job.jobTitle}
+            </p>
+          ))}
+        </div>
+      ) : (
+        <EmptyState
+          title="No jobs found"
+          description="Try searching for a different job title or location."
+          buttonText="Clear all filters"
+          href="/"
+        />
+      )}
+    </>
+  );
+};
+
+export default JobListing;
